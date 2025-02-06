45c07cefaeee93ed93ed2e634bdfbb71a2ce8bcc
Calcifer Kim <seeah.kim@cooper.edu>
2025-02-06 15:09:36 -0500

    added Rungekutta

--- HW/HW2/integrators.py
+++ HW/HW2/integrators.py
@@ -26,4 +26,10 @@ class RungeKutta(Integrator):
         xh = intg.step(t, x, u) # Huen predictor step
         return x + (1/6)*self.dt * (self.f(t, x, u) + 2*self.f(0.5*t+self.dt, xe, u) + 2*self.f(0.5*t+self.dt, xh, u))
         
-
+class RungeKutta(Integrator):
+    def step(self, t, x, u):
+        k1 = self.f(t, x, u)
+        k2 = self.f(t + 0.5 * self.dt, x + 0.5 * self.dt * k1, u)
+        k3 = self.f(t + 0.5 * self.dt, x + 0.5 * self.dt * k2, u)
+        k4 = self.f(t + self.dt, x + self.dt * k3, u)
+        return x + (self.dt / 6) * (k1 + 2 * k2 + 2 * k3 + k4)
