73977edcc4dfe822c1233aa283f10f2b2ee347ad
myisha <myisha2326@gmail.com>
2025-02-06 15:01:36 -0500

    trial

--- HW/HW2/integrators.py
+++ HW/HW2/integrators.py
@@ -19,5 +19,11 @@ class Heun(Integrator):
         xe = intg.step(t, x, u) # Euler predictor step
         return x + 0.5*self.dt * (self.f(t, x, u) + self.f(t+self.dt, xe, u))
 
+class RungeKutta(Integrator):
+    def step(self, t, x, u):
+        intg = Euler(self.dt, self.f)
+        xe = intg.step(t, x, u) # Euler predictor step
+        xh = intg.step(t, x, u) # Huen predictor step
+        return x + (1/6)*self.dt * (self.f(t, x, u) + 2*self.f(0.5*t+self.dt, xe, u) + 2*self.f(0.5*t+self.dt, xh, u))
         
 
