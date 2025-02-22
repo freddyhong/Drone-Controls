218cbd4109c2fcb7db9cbfecc8bf8e3df0b4e7f5
myisha <myisha2326@gmail.com>
2025-02-21 21:54:07 -0500

    fixed

--- HW/HW2/Q3.py
+++ HW/HW2/Q3.py
@@ -1,26 +1,21 @@
 import sympy as sp
 
-# Define symbolic variables
+p, q, r = sp.symbols('p q r')  #
+dp, dq, dr = sp.symbols('dp dq dr')  #
+
 J1, J2, J3 = sp.symbols('J1 J2 J3')
-p, r = sp.symbols('p r')
-m1, m2, m3 = sp.symbols('m1 m2 m3')
+J = sp.Matrix([[J1, 0, 0], [0, J2, 0], [0, 0, J3]])
 
-# Define inertia matrix
-J = sp.diag(J1, J2, J3)
+omega = sp.Matrix([p, q, r])
 
-# Define angular velocity vector (pitch velocity q = 0)
-omega = sp.Matrix([p, 0, r])
+omega_dot = sp.Matrix([dp, dq, dr])
 
-# Define external torque vector
-torque = sp.Matrix([m1, m2, m3])
+J_omega_dot = J * omega_dot
 
-# Compute angular momentum
-h = J * omega
+cross_product = omega.cross(J * omega)
 
-# Compute Euler's equations: J * d(omega)/dt + omega x (J * omega) = torque
-omega_dot = J.inv() * (torque - omega.cross(h))
+m_b = sp.Matrix(sp.symbols('m1^b m2^b m3^b'))
 
-# Display the equations
-print("Angular acceleration equations (dp/dt, dq/dt, dr/dt):")
-sp.pprint(omega_dot)
+euler_equation = sp.Eq(J_omega_dot + cross_product, m_b)
 
+sp.pprint(euler_equation)
