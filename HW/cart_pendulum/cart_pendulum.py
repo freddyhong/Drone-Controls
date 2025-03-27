3b2f53c7d25c1b0b6e3e9ff62b3ff6a5958f8a71
myisha <myisha2326@gmail.com>
2025-03-27 14:30:52 -0400

    upload

---
+++ HW/cart_pendulum/cart_pendulum.py
@@ -0,0 +1,35 @@
+import numpy as np
+import control as ct
+
+# Parameters
+M = 0.5
+m = 0.2
+b = 0.1
+I = 0.006
+g = 9.8
+l = 0.3
+
+# State space
+den = I*(M+m)+M*m*l**2
+A = np.array([
+    [0,      1,              0,           0],
+    [0, -(I+m*l**2)*b/den,  (m**2*g*l**2)/den,  0],
+    [0,      0,              0,           1],
+    [0, -(m*l*b)/den,       m*g*l*(M+m)/den,  0]
+    ])
+B = np.array([
+    [0],
+    [(I+m*l**2)/den],
+    [0],
+    [m*l/den]
+    ])
+C = np.array([
+    [1, 0, 0, 0],
+    [0, 0, 1, 0]
+    ])
+D = np.array([
+    [0],
+    [0]
+    ])
+
+print(A)
